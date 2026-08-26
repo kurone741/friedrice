0f2ad73faf6fd5f6cefa843cb8361dbc5cb21f79
liangnb07 <liangnb07@proton.me>
2026-08-26 15:53:17 -0500

    autostart awww

--- desktop/hypr/autostart.lua
+++ desktop/hypr/autostart.lua
@@ -1,3 +1,6 @@
+local wallpaperDir = "~/.config/walls"
 
 hl.dsp.exec_cmd("waybar")
 hl.dsp.exec_cmd("awww-daemon")
+
+hl.dsp.exec_cmd("awww-daemon & sleep 0.2; awww img \"$(find " .. wallpaperDir .. " -type f | shuf -n 1)\"")
--- desktop/hypr/keybinds.lua
+++ desktop/hypr/keybinds.lua
@@ -2,12 +2,18 @@ local mainMod = "SUPER"
 local terminal = "kitty"
 local menu = "rofi -show drun"
 
-hl.bind(mainMod .. " + M", hl.dsp.exit())
+--wlogout when super m
+hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("wlogout"))
 
+--kitty when super space
 hl.bind(mainMod .. " + Space", hl.dsp.exec_cmd(terminal))
 
+--exit when super q
 hl.bind(mainMod .. " + Q", function()
-    hl.dispatch(hl.dsp.window.kill())
+    hl.dispatch(hl.dsp.window.close())
 end)
 
+--rofi(run) when super r
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
+
+--
